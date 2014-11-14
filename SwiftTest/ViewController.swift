984f89719c8428323cac8de5e0d653c5fbe0b42d
xubojoy <xubojoy1112@163.com>
2014-11-14 13:50:14 +0800

    添加

--- SwiftTest/ViewController.swift
+++ SwiftTest/ViewController.swift
@@ -13,6 +13,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var label = UILabel  (frame: CGRectMake(100, 100, 100,100))
+        label.backgroundColor = UIColor.purpleColor()
+        label.textAlignment = NSTextAlignment.Center
+        label.text = "hello world"
+        label.textColor=UIColor.blueColor()
+        self.view.addSubview(label)
+        
+        var btn  = UIButton (frame: CGRectMake(100, 300, 100, 100));
+        btn.addTarget(self, action: "btnClick", forControlEvents: UIControlEvents.TouchUpInside)
+        btn.backgroundColor = UIColor.redColor()
+        btn.setTitle("点击", forState: UIControlState.Normal)
+        self.view.addSubview(btn)
+        
+        test()
+        
+        
+    }
+    
+    func btnClick(){
+        
+        println(">>>>>>>>>>>>点击啦")
+    
+        NSLog("》》》》》》》点击了")
+    
+    }
+    
+    func test(){
+    
+        println(">>>>>>>>>>>>测试啦！！！！")
+    
     }
 
     override func didReceiveMemoryWarning() {
