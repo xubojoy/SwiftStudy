941c034fc505428fc9def1a5bd8c9f1c31b42db7
xubojoy <xubojoy1112@163.com>
2014-11-20 18:39:46 +0800

    提交

--- SwiftStudy-02/SwiftStudy-02/ThirdViewController.swift
+++ SwiftStudy-02/SwiftStudy-02/ThirdViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ThirdViewController: UIViewController {
+class ThirdViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    var tableView : UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.cyanColor()
+        
+        self.tableView = UITableView (frame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height-64), style: UITableViewStyle.Plain)
+        self.tableView!.delegate = self;
+        self.tableView!.dataSource = self;
+        self.tableView!.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.view.addSubview(self.tableView!)
 
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView .dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel.text = "好啊"
+        return cell
+        
     }
    
     override func didReceiveMemoryWarning() {
